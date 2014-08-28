b507a9a8723531098803a378a60c468f7361aab9
Nick Schuch <nick@myschuch.com>
2014-08-28 13:10:53 +1000

    Initial commit.

---
+++ src/app.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Silex\Application;
+use Guzzle\GuzzleServiceProvider;
+
+$app = new Application();
+
+// Services.
+$app->register(new GuzzleServiceProvider(), array(
+  'guzzle.services' => '/path/to/services.json',
+));
+
+// Routes.
+$app->get('/travis', function($name) use($app) {
+  return 'You wanna test this action?';
+});
+
+$app->run();
