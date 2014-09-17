4f84b3267b3acec7bb4ded2d6e61743509fe8712
Nick Schuch <nick@myschuch.com>
2014-09-17 21:27:14 +1000

    First pass.

--- tests/JenkinsTest.php
+++ tests/JenkinsTest.php
@@ -10,24 +10,38 @@ class JenkinsTest extends \PHPUnit_Framework_TestCase {
 
   /**
    * Build a Jenkins object and get the URL that will be used for submission.
+   * @expectedException Exception
    */
   public function testBuildUrl() {
+    $this->setExpectedException('Exception');
+
     // Build.
     $guzzle = new GuzzleMock();
     $jenkins = new Jenkins($guzzle);
     $jenkins->setProtocol('https');
+
+    // Check for enforcement of host.
+    $request = $jenkins->send();
     $jenkins->setHost('localhost');
+
+    // Check for enforcement of build.
+    $request = $jenkins->send();
+    $jenkins->setBuild('foo');
+
+    // Check for enforcement of port.
+    $request = $jenkins->send();
     $jenkins->setPort('9090');
+
+    // Add the rest so we can test a successful return.
     $jenkins->setToken('99999999');
     $jenkins->setQuery(array(
       'repository' => 'baz',
       'branch' => 'bar',
       'patch' => 'bas'
     ));
-    $jenkins->setBuild('foo');
     $request = $jenkins->send();
 
-    // Check.
+    // Check a successful submission.
     $required = array('https://localhost:9090/job/foo/buildWithParameters', array(
       'query' => array(
         'token' => '99999999',
@@ -37,13 +51,6 @@ class JenkinsTest extends \PHPUnit_Framework_TestCase {
       ),
     ));
     $this->assertEquals($required, $request);
-  }
-
-  /**
-   * Tests the data that will be sent to the Jenkins host via Guzzle.
-   */
-  public function testSend() {
-
   }
 
 }
