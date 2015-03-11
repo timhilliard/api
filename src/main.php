bdcc39e0b3c03c6e16f92ecf349275c2ff95c1b3
Tim Hilliard <tim.hilliard@acquia.com>
2015-03-11 22:11:15 +1000

    Add basic auth

--- src/main.php
+++ src/main.php
@@ -6,6 +6,7 @@ $loader->add('', __DIR__);
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 use DerAlex\Silex\YamlConfigServiceProvider;
 use Silex\Application;
 use Silex\Provider\SecurityServiceProvider;
@@ -17,10 +18,10 @@ $app = new Silex\Application();
  * Environment.
  */
 if (file_exists('config.yaml')) {
-  $config = '../config/config.yaml';
+    $config = '../config/config.yaml';
 }
 else {
-  $config = '../config/config-test.yaml';
+    $config = '../config/config-test.yaml';
 }
 
 /**
@@ -32,25 +33,52 @@ $app->register(new YamlConfigServiceProvider($config));
  * Handling.
  */
 $app->error(function (\Exception $e, $code) {
-  error_log($e);
-  return "Something went wrong. Please contact the DrupalCI team.";
+    error_log($e);
+    return "Something went wrong. Please contact the DrupalCI team.";
 });
 
 if ($app['config']['log-file']) {
-  $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' =>  $app['config']['log-file'],
-    'monolog.name' => 'Drupal CI API',
-  ));
+    $app->register(new Silex\Provider\MonologServiceProvider(), array(
+        'monolog.logfile' =>  $app['config']['log-file'],
+        'monolog.name' => 'Drupal CI API',
+    ));
 }
+
+/**
+ * Build out our users and give them appropriate roles.
+ */
+$encoder = new MessageDigestPasswordEncoder();
+$users = array();
+foreach ($app['config']['users'] as $username => $password) {
+    $users[$username] = array(
+        'ROLE_USER',
+        $encoder->encodePassword($password, ''),
+    );
+}
+
+// Security definition.
+$app->register(new SecurityServiceProvider());
+$app['security.firewalls'] = array(
+    // Login URL is open to everybody.
+    'default' => array(
+        'pattern' => '^.*$',
+        'http' => true,
+        'stateless' => true,
+        'users' => $users,
+    ),
+);
+$app['security.access_rules'] = array(
+    array('^.*$', 'ROLE_USER'),
+);
 
 /**
  * Routing.
  */
 $app['routes'] = $app->extend('routes', function (RouteCollection $routes, Application $app) {
-  $loader = new YamlFileLoader(new FileLocator(__DIR__));
-  $collection = $loader->load('routes.yml');
-  $routes->addCollection($collection);
-  return $routes;
+    $loader = new YamlFileLoader(new FileLocator(__DIR__));
+    $collection = $loader->load('routes.yml');
+    $routes->addCollection($collection);
+    return $routes;
 });
 
 $app->run();
